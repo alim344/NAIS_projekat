8679745e00179bca7b32069b10e3abece2c86516
alim344 <milabudimirovic2@gmail.com>
2026-04-26 22:53:41 +0200

    Class Organization initial service

---
+++ class-organization/src/main/java/com/example/class_organization/ClassOrganizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.class_organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassOrganizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassOrganizationApplication.class, args);
+	}
+
+}
