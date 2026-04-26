c8c38432246be326586df13d95c88fca13122235
alim344 <milabudimirovic2@gmail.com>
2026-04-26 22:53:41 +0200

    Class Organization initial service

---
+++ class-organization/src/test/java/com/example/class_organization/ClassOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.class_organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassOrganizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
